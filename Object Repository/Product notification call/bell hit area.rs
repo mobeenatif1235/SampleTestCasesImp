709b98b553c983a49099799caa0a89aa981484dd
Computer <Computer@DESKTOP-JDK6SO4>
2018-09-26 00:39:42 -0700

    Production Notification get call

---
+++ Object Repository/Product notification call/bell hit area.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>bell hit area</name>
+   <tag></tag>
+   <elementGuidId>ce383b4a-be8d-4087-b7af-024bb794279c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//span[@class=&quot;bell-hit-area&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//span[@class=&quot;bell-hit-area&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
--- Scripts/Product Notification get call/Script1537946137745.groovy
+++ Scripts/Product Notification get call/Script1537946137745.groovy
@@ -10,19 +10,8 @@ import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.interactions.Actions
-import com.kms.katalon.core.webui.driver.DriverFactory
-import org.openqa.selenium.By
-import org.openqa.selenium.Keys
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.interactions.Actions
-import com.kms.katalon.core.webui.driver.DriverFactory
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.sun.media.controls.ActionControl
 
 WebUI.openBrowser('')
 WebUI.navigateToUrl("https://app.convo.com/app/login/?redirect=https%3A%2F%2Fapp.convo.com%2F")
@@ -38,19 +27,16 @@ WebUI.sendKeys(findTestObject('Object Repository/Login Objects/Password'), "Kabu
 
 WebUI.click(findTestObject('Object Repository/Login Objects/Loginbutton'))
 
-
-WebUI.waitForElementVisible(findTestObject('Object Repository/Public and Private groups/Private group array'), 40)
-WebUI.waitForElementVisible(findTestObject('Object Repository/Public and Private groups/Public group array'), 40)
-
 WebUI.delay(20)
-WebDriver driver=DriverFactory.getWebDriver()
 
-Privategrpscount= driver.findElements(By.xpath("//li[contains(@bo-repeat,'group in privateGroupsArray')]")).size()
-println "Private Groups count is: " + Privategrpscount
+WebUI.waitForElementVisible(findTestObject('Object Repository/Product notification call/bell hit area'), 30)
 
-Publicgrpscount=driver.findElements(By.xpath("//li[contains(@bo-repeat,'group in publicGroupsArray')]")).size()
-println "Public Groups count is: " + Publicgrpscount
+
+WebUI.click(findTestObject('Object Repository/Product notification call/bell hit area'))
+
 
 WebUI.closeBrowser()
+
+
 
 
