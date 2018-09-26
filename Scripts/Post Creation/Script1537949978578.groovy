618c9d3f2cabdb35d2f9a0bfb784b21a27be19a7
Computer <Computer@DESKTOP-JDK6SO4>
2018-09-26 05:21:03 -0700

    This is Post Creation test case

--- Scripts/Post Creation/Script1537949978578.groovy
+++ Scripts/Post Creation/Script1537949978578.groovy
@@ -1,4 +1,16 @@
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import java.sql.Timestamp
+
+import org.openqa.selenium.Keys
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
@@ -8,6 +20,7 @@ import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.driver.DriverFactory
@@ -77,4 +90,34 @@ public String getTimeStamp(){
 
 WebUI.delay(20)
 
+
+// Creating the Test Object to locate the created post. (),Post Indexing
+
+TestObject newPostCreated = new TestObject('new post created')
+String xpathNewPostCreated = '//span/p[contains(text(), "' + timestamp + '")]'
+
+newPostCreated.addProperty('xpath', ConditionType.EQUALS, xpathNewPostCreated, true)
+
+//Wait for test post created to be visible.
+WebUI.waitForElementVisible(newPostCreated, 60)
+
+//To verify new post created is present.
+WebUI.verifyElementPresent(newPostCreated, 30)
+
+
+WebUI.delay(20)
+
+TestObject newPostCreated2 = new TestObject('newly post created')
+String xpathPostSharedTitleText = "//div/a[contains(text(),'" + timestamp + "')]"
+newPostCreated2.addProperty('xpath', ConditionType.EQUALS, xpathPostSharedTitleText, true)
+
+WebUI.waitForElementVisible(newPostCreated2, 60)
+
+//To verify new post created is present.
+WebUI.verifyElementPresent(newPostCreated2, 30)
+
+WebUI.click(newPostCreated2)
+
+
+WebUI.delay(30)
 WebUI.closeBrowser()
